db6c1c4f83a6954af55e34663c8a2af239fa5656
zapiorpatryk <zapior.patryk@gmail.com>
2015-07-27 12:07:37 +0200

    Directives

--- app/mouse.js
+++ app/mouse.js
@@ -1,3 +1,34 @@
 'use strict';
 
 var app = angular.module('app', []);
+
+app.directive("enter", function(){
+    return function ($scope, element, attr){
+        element.bind("mouseenter", function(){
+            element.addClass(attr.enter);
+        })
+    }
+})
+app.directive("leave", function(){
+    return function ($scope, element, attr){
+        element.bind("mouseleave", function(){
+            element.removeClass(attr.enter);
+        })
+    }
+})
+
+app.directive("dblClickOn", function(){
+    return function($scope, element, attr){
+        element.bind("dblclick", function(){
+            element.addClass(attr.dblClickOn);
+        })
+    }
+})
+
+app.directive("clickOn", function(){
+    return function($scope, element, attr){
+        element.bind("click", function(){
+            element.removeClass(attr.dblClickOn);
+        })
+    }
+})
