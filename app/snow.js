7267bed9a6a0abc7298a4920b60478fe1eaa3ebc
zapiorpatryk <zapior.patryk@gmail.com>
2015-07-30 14:08:06 +0200

    Directives talking to controllers

--- app/snow.js
+++ app/snow.js
@@ -6,8 +6,22 @@ app.controller('SnowCtrl', function ($scope)
 {
     $scope.snow = [];
 
+    $scope.addPetal = function(){
+        $scope.snow.push({petal: '*'});
+    };
+    $scope.removePetal = function(){
+        $scope.snow.splice(0,1);
+    };
+
 //    addPetal function
 //    removePetal function
 });
 
 //    add enter directive
+app.directive("enter", function(){
+    return function (scope, element, attr){
+        element.bind("mouseenter", function(){
+            scope.$apply(attr.enter);
+        })
+   }
+});
