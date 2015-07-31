6c36ad75932f4d580f20752aa841c4c0584459b1
zapiorpatryk <zapior.patryk@gmail.com>
2015-07-31 10:20:13 +0200

    Directive to Directive Communication 1

--- app/alarm.js
+++ app/alarm.js
@@ -1,6 +1,55 @@
 var app = angular.module('app', []);
 
-//<!--add alarm directive-->
-//<!--add red directive-->
-//<!--add yellow directive-->
-//<!--add green directive-->
+app.controller("AppCtrl", function($scope) {
+
+    $scope.addRed = function () {
+        alert("Alarm red!")
+    },
+    $scope.addYellow = function () {
+        alert("Alarm yellow!")
+    },
+    $scope.addGreen = function () {
+        alert("Alarm green!")
+    }
+}),
+
+
+app.directive("alarm", function(){
+    return{
+        restrict: 'A',
+        scope: {
+            addRed: '&',
+            addYellow: '&',
+            addGreen: '&',
+        },
+    }
+});
+
+    app.directive("red", function(){
+    return{
+        link: function(scope, element){
+            element.bind("click", function(){
+                scope.addRed();
+            })
+        }
+    }
+}),
+
+    app.directive("yellow", function(){
+        return{
+            link: function(scope, element){
+                element.bind("dblclick", function(){
+                    scope.addYellow();
+                })
+            }
+        }
+}),
+    app.directive("green", function(){
+        return{
+            link: function(scope, element){
+                element.bind("mouseenter", function(){
+                    scope.addGreen();
+                })
+            }
+        }
+});
