53e172772bfbfb22ac166f05f86225b3b0002551
zapiorpatryk <zapior.patryk@gmail.com>
2015-07-31 14:50:06 +0200

    An alternative approach to controllers

--- app/calc.js
+++ app/calc.js
@@ -2,5 +2,26 @@
 
 var calcApp = angular.module('calcApp', []);
 
-//SumCtrl
-//SubCtrl
+calcApp.controller("SumCtrl", function($scope){
+    this.firstNumber = 12;
+    this.secondNumber = 1 ;
+
+    this.addition = function(firstNumber, secondNumber){
+        console.log(firstNumber + secondNumber);
+        return firstNumber + secondNumber;
+
+    }
+    return $scope.SumCtrl = this;
+
+})
+
+calcApp.controller("SubCtrl", function($scope){
+    this.firstNumber = 12;
+    this.secondNumber = 1;
+
+    this.subtraction = function(firstNumber,secondNumber){
+        return firstNumber - secondNumber;
+        //return sub;
+    }
+    return $scope.SubCtrl = this;
+})
