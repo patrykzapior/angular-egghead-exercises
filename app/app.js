6c30d15c1e9c91e983aaf5361736e49434223f83
zapiorpatryk <zapior.patryk@gmail.com>
2015-08-05 09:14:23 +0200

    secret

--- app/app.js
+++ app/app.js
@@ -5,6 +5,40 @@
     var app = angular.module('app', ['ngRoute']);
 
     // add routing here
+    app.config(function ($routeProvider){
+        $routeProvider
+            .when('/',
+            {
+                templateUrl: 'views/home.html',
+                controller: 'homeCtrl'
+            })
+            .when('/details/:brand/:capacity',
+            {
+                templateUrl: 'views/details.html',
+                controller: 'routeCtrl'
+            }).when('/secret',
+            {
+                templateUrl: 'views/secret.html',
+                controller: 'routeCtrl'
+            })
+            .when('/inputs/:brand/:capacity',
+            {
+                redirectTo: function(routeParams,path,search){
+                    var secret = path.split('/').indexOf('secret');
+                    if(secret !==-1){
+                        return '/' + 'secret';
+                    }
+                    else if(routeParams.brand && routeParams.capacity){
+                        return '/' + 'details'+'/'+routeParams.brand+'/'+routeParams.capacity;
+                    }
+                    else {
+                        return '/';
+                    }
+                }
+            }).otherwise({
+                redirectTo: '/'
+            })
+    })
 
     app.controller('homeCtrl', function ($scope)
     {
