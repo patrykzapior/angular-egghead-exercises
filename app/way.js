7db07635e0a9d5d995aab924eb57c266c00a7b8d
zapiorpatryk <zapior.patryk@gmail.com>
2015-08-06 09:06:50 +0200

    Directive for route handling

--- app/way.js
+++ app/way.js
@@ -24,24 +24,39 @@ wayApp.config(function ($routeProvider) {
 });
 
 // add error directive here
+wayApp.directive("error", function($rootScope) {
+    return {
+        restrict: "E",
+        template: '<div class="alert alert-danger" ng-if="wrongWay">No, no, no. That is not good way!</div>',
+        link: function(scope) {
+            $rootScope.$on("$routeChangeError", function(event, current, previous, rejection) {
+                console.log(event)
+                console.log(current)
+                console.log(previous)
+                console.log(rejection)
+                scope.wrongWay = true;
+            })
+        }
+    }
+})
 
 var WayCtrl = wayApp.controller('WayCtrl', function ($scope, $route) {
     $scope.alertMessage = 'You choose the right way! :)';
 });
 
-WayCtrl.error = function ($q, $timeout) {
-    var defer = $q.defer();
-    $timeout(function () {
-        defer.reject();
-    }, 500);
-    return defer.promise;
-};
+    WayCtrl.error = function ($q, $timeout) {
+        var defer = $q.defer();
+        $timeout(function () {
+            defer.reject();
+        }, 500);
+        return defer.promise;
+    };
 
-WayCtrl.success = function ($rootScope, $q, $timeout) {
-    var defer = $q.defer();
-    $rootScope.wrongWay = false;
-    $timeout(function () {
-        defer.resolve();
-    }, 100);
-    return defer.promise;
+    WayCtrl.success = function ($rootScope, $q, $timeout) {
+        var defer = $q.defer();
+        $rootScope.wrongWay = false;
+        $timeout(function () {
+            defer.resolve();
+        }, 100);
+        return defer.promise;
 };
