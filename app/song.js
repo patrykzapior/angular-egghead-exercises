14874f6c2619ee8336c45991f63f9afeca576314
zapiorpatryk <zapior.patryk@gmail.com>
2015-08-10 10:40:19 +0200

    Injectors-1

--- app/song.js
+++ app/song.js
@@ -5,21 +5,28 @@ var songApp = angular.module('songApp', []);
 songApp.factory('Song', function ()
 {
     return {
-        author: 'Gotye feat. Kimbra', title: 'Somebody That I Used To Know', sing: function ()
+        author: 'Gotye feat. Kimbra',
+        title: 'Somebody That I Used To Know',
+
+        sing: function ()
         {
             return 'But you did not have to cut me off... But you treat me like a stranger ...';
-        }, fake: function ()
+        },
+        fake: function ()
         {
             return 'Buuuuuuuuuuuuuut you niah niah blah bla ...';
         }
     };
 });
 
-songApp.controller('SongCtrl', function (Song)
+songApp.controller('SongCtrl', function ($scope, $injector)
 {
     var ctrl = this;
-    ctrl.author = Song.author;
-    ctrl.title = Song.title;
-    ctrl.sing = Song.sing();
-    ctrl.fake = Song.fake();
+
+    $injector.invoke (function(Song){
+        ctrl.author = Song.author;
+        ctrl.title  = Song.title;
+        ctrl.sing   = Song.sing();
+        ctrl.fake   = Song.fake();
+    })
 });
